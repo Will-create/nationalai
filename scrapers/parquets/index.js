b91a8ee2e909f4593eab158c26bc5061a41ba559
Louis Bertson <louisbertson@gmail.com>
2023-12-31 12:24:42 +0000

    Added package for parquet

--- scrapers/parquets/index.js
+++ scrapers/parquets/index.js
@@ -1,25 +1,25 @@
 // Downloaded from https://huggingface.co/datasets/Wabbina/moore_dataset_fr_translation_v1.0/blob/main/data/train-00000-of-00001-0252c1088531b372.parquet
 var PARQUET_PATH = '/Users/mac/Downloads/train-00000-of-00001-0252c1088531b372.parquet';
-const parquet = require('parquetjs');
+const parquet = require('@dsnp/parquetjs');
 const LIMIT_ROWS = 50;
-var reader = await parquet.ParquetReader.openFile(PARQUET_PATH);
- 
-// create a new cursor
-var cursor = reader.getCursor();
- 
-// read all records from the file and print them
-var record = null;
-var index = 0;
 
-async function main () {
 
+
+parquet.ParquetReader.openFile(PARQUET_PATH).then(async function (reader) {
+    var cursor = reader.getCursor();
+
+    // read all records from the file and print them
+    var record = null;
+    var index = 0;
     while (record = await cursor.next()) {
         index += 1;
 
         console.log(record);
 
-
         if (index === LIMIT_ROWS)
             break;
     }
-}
+}).catch(function (err) {
+    console.log(err);
+});
+
