ab825282a2599c1a39a82941438bec1228a718f3
Louis Bertson <louisbertson@gmail.com>
2024-01-01 19:21:33 +0000

    Created parquet dataset

---
+++ scrapers/parquets/fromjson.py
@@ -0,0 +1,27 @@
+import pandas as pd
+
+json_path1 = 'output/webonary.json'  # Remplace avec ton chemin du fichier JSON
+json_path2 = 'output/lexique.json'  # Remplace avec ton chemin du fichier JSON
+parquet_path = 'output/dataset.parquet'  # Remplace avec ton chemin de sortie souhaité
+
+def json_to_parquet():
+    try:
+        # Charger les données JSON
+        df = pd.read_json(json_path1, lines=False)
+        df2 = pd.read_json(json_path2, lines=False)
+
+        # combine the 
+        df_combined = pd.concat([df, df2])
+
+        # View it
+        print(df_combined)
+
+        # Sauvegarde en format Parquet
+        df_combined.to_parquet(parquet_path)
+        
+        print("C'est fait, check ton fichier Parquet! 🚀")
+    except Exception as e:
+        print(f"Oops, petit souci: {e} 😬")
+
+# Exécution de la fonction
+json_to_parquet()
