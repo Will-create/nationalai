118492ec337387daac7be6c07b13cd2f7a0868e1
Louis Bertson <louisbertson@gmail.com>
2024-01-02 23:55:48 +0000

    Splitted dataset

--- scrapers/parquets/fromjson.py
+++ scrapers/parquets/fromjson.py
@@ -1,5 +1,8 @@
 import pandas as pd
 
+lexique_path = 'output/lexique.json'  # Remplace avec ton chemin du fichier JSON
+webonary_path = 'output/webonary.json'  # Remplace avec ton chemin du fichier JSON
+translate_path = 'output/translation.json'  # Remplace avec ton chemin du fichier JSON
 json_path1 = 'output/translation2.json'  # Remplace avec ton chemin du fichier JSON
 json_path2 = 'output/translation3.json'  # Remplace avec ton chemin du fichier JSON
 json_path3 = 'output/translation4.json'  # Remplace avec ton chemin du fichier JSON
@@ -10,6 +13,9 @@ parquet_path = 'output/dataset.parquet'  # Remplace avec ton chemin de sortie so
 def json_to_parquet():
     try:
         # Charger les données JSON
+        df_1 = pd.read_json(lexique_path, lines=False)
+        df_2 = pd.read_json(webonary_path, lines=False)
+        df_3 = pd.read_json(translate_path, lines=False)
         df = pd.read_json(json_path1, lines=False)
         df2 = pd.read_json(json_path2, lines=False)
         df3 = pd.read_json(json_path3, lines=False)
@@ -17,7 +23,7 @@ def json_to_parquet():
         df5 = pd.read_json(json_path5, lines=False)
 
         # combine the 
-        df_combined = pd.concat([df, df2, df3, df4, df5])
+        df_combined = pd.concat([df_1, df_2, df_3, df, df2, df3, df4, df5])
 
         # View it
         print(df_combined)
