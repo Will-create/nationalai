9fcac31709056fd7dfc09485c3e194b5d4ca091f
Louis Bertson <louisbertson@gmail.com>
2024-01-05 13:30:51 +0000

    Added script to send raamde-bf data to todomator for manual processing

---
+++ scrapers/raamde-bf.net/toserver.js
@@ -0,0 +1,37 @@
+require('total5');
+
+var path = 'output/raamde-bf_draft.json';
+var server = '';
+var token = '';
+var folder = 'Language models';
+var ownerid = 'IW5XDs1cD61f';
+var users = ['chris@totaljs.com', 'fadilah@gmail.com', 'kienteganourdine72@gmail.com', 'louisbertson@gmail.com', 'ouedjosa47@gmail.com'];
+F.Fs.readFile(path, 'utf-8', function(err, response) {
+
+    var items = response.parseJSON(true);
+
+    var index = 0;
+    items.wait(function(item, next) {
+        index++;
+
+        item.name = 'Text ' + index;
+        item.dtcreated = NOW;
+        item.complete = false;
+        
+        var ticket = {};
+
+        ticket.name = item.name;
+        ticket.translate1 = item.value;
+        ticket.folder = folder;
+        ticket.ownerid = ownerid;
+        ticket.istranslate = true;
+        ticket.users = users;
+
+        RESTBuilder.POST(server, { schema: 'tickets_create', data: ticket }).header('x-token', token).keepalive().callback(function(err, res) {
+            console.log(err, res);
+            next();
+        })
+    }, function() {
+        console.log('Done');
+    });
+});
